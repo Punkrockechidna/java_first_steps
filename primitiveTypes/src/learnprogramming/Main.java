6955435034acdcf11383e5e9a8e4a37b0625e11a
Punkrockechidna <dan.kaplan@ieleadership.org>
2019-12-07 23:31:47 -0500

    started primitive types

---
+++ primitiveTypes/src/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package learnprogramming;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myValue = 10000;
+		int myMinIntValue = Integer.MIN_VALUE;
+		int myMaxIntValue = Integer.MAX_VALUE;
+		System.out.println("Integer Min:" + myMinIntValue);
+		System.out.println("Integer Max:" + myMaxIntValue);
+		System.out.println("Busted max:" + (myMaxIntValue + 1));
+
+		int myMaxIntTest = 2_147_483_647; // This is another way of writing numbers
+
+	}
+
+}
---
+++ src/helloWorld/Hello.java
@@ -0,0 +1,19 @@
+package helloWorld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, Ava");
+
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber * 2;
+
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+		int myLastOne = 1000 - myTotal;
+		System.out.println(myLastOne);
+//	shortcut to get this is to use sout then autocomplete	System.out.println();
+	}
+}
